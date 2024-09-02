0796a6d18c6ec96518154d31a20315bdd5aaabba
Kyrylo Kusovsky <kkirilll2006@gmail.com>
2024-09-02 23:42:05 +0300

    Add convertation file with decimal to binary convertor for labels

---
+++ convertation.py
@@ -0,0 +1,24 @@
+from tkinter import END
+import tkinter.messagebox as ms
+
+def decimal_to_binary_lab(entry):
+    try: 
+        dec = int(entry.get())
+        result_to_print = ""
+        minus = True if dec < 0 else False
+        dec = abs(dec)
+        if dec == 0:
+            return str(0)
+        
+        while dec!=0:
+            result_to_print += str(dec%2)
+            dec = dec//2
+        entry.delete(0, END)
+        return '-' + result_to_print[::-1] if minus else result_to_print[::-1]
+    except TypeError:
+        ms.showerror("Error!", "Enter integer number")
+        entry.delete(0, END)
+    except ValueError:
+        ms.showerror("Error!", "Enter integer number")
+        entry.delete(0, END)
+    
