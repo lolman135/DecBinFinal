223809a6d6f9516872693cd3ce66653ef729a44c
Kyrylo Kusovsky <kkirilll2006@gmail.com>
2024-09-02 23:54:24 +0300

    Add menu for main window

--- main.py
+++ main.py
@@ -20,8 +20,22 @@ def convert_win(title, type, func):
 
     win2.mainloop()
 
+
+
 root = Tk()
 root.title("DecBin")
 root.minsize(width=300, height=200)
 
+choice_label = Label(root, text = "Choose operation", font="Arial 20")
+choice_label.pack(pady=10)
+menu_frame = Frame(root)
+menu_frame.pack(fill=BOTH, expand=True)
+dec_to_bin_button = Button(menu_frame, text = "Decimal to Binary", width=15, command=None)
+bin_to_dec_button = Button(menu_frame, text = "Binary to Decimal", width=15, command=None)
+calculator_button = Button(menu_frame, text = "Arithmetic operations", width=15, command=None)
+dec_to_bin_button.place(relx=0.1, rely=0.15, relwidth=0.8, relheight=0.2)
+bin_to_dec_button.place(relx=0.1, rely=0.4, relwidth=0.8, relheight=0.2)
+calculator_button.place(relx=0.1, rely=0.65, relwidth=0.8, relheight=0.2)
+
+menu_frame.anchor("center")
 root.mainloop()
