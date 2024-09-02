093077bd0ae081d54133e48876a0a326190c8af2
Kyrylo Kusovsky <kkirilll2006@gmail.com>
2024-09-03 00:10:36 +0300

    Add calculating function

--- operation.py
+++ operation.py
@@ -17,5 +17,45 @@ def convert_to_binary(result):
     while result!=0:
         result_to_print += str(result%2)
         result =result//2
-        
+
     return '-' + result_to_print[::-1] if minus else result_to_print[::-1]
+
+#mos - mathematical operation sign
+def calculating(entry1, entry2, mos):
+    try:
+        num1, num2 = convert_to_decimal(entry1.get(), entry2.get())
+        flag = True
+        match(mos.get()):
+            case "+":
+                result = num1 + num2
+            case "-":
+                result = num1 - num2
+            case "*":
+                result = num1 * num2
+            case "/":
+                result = num1 // num2
+            case _:
+                result = "wrong operation!"
+                flag = False
+        if flag:
+            entry1.delete(0, END)
+            entry2.delete(0, END)
+            mos.delete(0, END)
+            return convert_to_binary(result)
+            
+        else:
+            entry1.delete(0, END)
+            entry2.delete(0, END)
+            mos.delete(0, END)
+            return result
+    
+    except TypeError:
+        ms.showerror("Error!", "Enter binary number")
+        entry1.delete(0, END)
+        entry2.delete(0, END)
+        mos.delete(0, END)
+    except ValueError:
+        ms.showerror("Error!", "Enter binary number")
+        entry1.delete(0, END)
+        entry2.delete(0, END)
+        mos.delete(0, END)
